76d888dc40c58f789f804b62d03f6d34181b7c2d
Egorych <mikhalevichegor@gmail.com>
2022-11-08 19:37:46 +0300

    changed the props and transferred them to the map

---
+++ src/components/Tasks.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {DataType} from "../App";
+
+
+type TasksProsType = {
+    data: DataType
+}
+
+
+export function Tasks(props: TasksProsType) {
+
+    const taskList = props.data.tasks.map( (el) => {
+        return (
+            <li key={el.taskId}><input type='checkbox' checked={el.isDone}/>
+                <span>{el.title}</span>
+            </li>
+        )
+    })
+    return (
+        <div>
+            <h1>{props.data.title}</h1>
+            <ul>
+                {taskList}
+            </ul>
+        </div>
+    )
+}
